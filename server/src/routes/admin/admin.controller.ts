ab5313454c10ceb87bcbb4bb9c24b8df41e6b10e
nasaee_window <nasaee.dev@gmail.com>
2024-03-01 00:45:37 +0700

    backend move admin handle delete user request to controller

--- server/src/routes/admin/admin.controller.ts
+++ server/src/routes/admin/admin.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { getAllUsersDB } from '../../models/user/user.model';
+import { deleteUserById, getUserById } from '../../models/user/user.model';
 
 export const getAllUsers = async (req: Request, res: Response) => {
   const allUsers = await getAllUsersDB();
@@ -7,4 +7,25 @@ export const getAllUsers = async (req: Request, res: Response) => {
     return res.status(500).send({ message: 'Somthing went wrong' });
   }
   return res.status(200).send(allUsers);
+};
+
+export const deleteUserById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(500).send({ message: 'bed request' });
+  }
+  const user = await getUserById(id);
+
+  if (!user) {
+    return res.status(500).send({ message: 'user ID not exist' });
+  }
+  try {
+    await deleteUserById(id);
+    res.status(200).send({ message: 'user has been deleted' });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: 'Somthing went wrong, user not logged out' });
+  }
 };
--- server/src/routes/v1.ts
+++ server/src/routes/v1.ts
@@ -5,11 +5,11 @@ import verifyToken from '../middlewares/verifyToken.middleware';
 import adminRouter from './admin/admin.route';
 import checkUserRole from '../middlewares/checkUserRole';
 
-const api = express.Router();
+const apiV1 = express.Router();
 
 // /api/v1/...
-api.use('/auth', authRouter);
-api.use('/users', usersRouter);
-api.use('/admin', verifyToken, checkUserRole('admin'), adminRouter);
+apiV1.use('/auth', authRouter);
+apiV1.use('/users', usersRouter);
+apiV1.use('/admin', verifyToken, checkUserRole('admin'), adminRouter);
 
-export default api;
+export default apiV1;
